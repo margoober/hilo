5086d591f503fa71bfdfe31342dce63a7597dd40
Margot McMahon <andlmcm@Andrews-MacBook-Air.local>
2016-02-29 13:54:21 -0600

    Finished HiLo Game

--- hilo.php
+++ hilo.php
@@ -1,11 +1,26 @@
 <?PHP
-$rando = rand(1, 100);
-echo "Comp chosed {$rando}" . PHP_EOL;
-$userChoice;
-if ($userChoice == $rando) {
-	echo "BULLSEYE!" . PHP_EOL;
-} else if ($userChoice > $rando) {
-	echo "Lower!" . PHP_EOL;
-} else {
-	echo "Higher!" . PHP_EOL;
+function game() {
+	$rando = rand(1, 10);
+do {
+	fwrite(STDOUT, 'Choose a number and press enter! ');
+	$userChoice = trim(fgets(STDIN));
+	fwrite(STDOUT, "Your choice were $userChoice\n");
+	if ($userChoice == $rando) {
+		echo "BULLSEYE!" . PHP_EOL;
+	} else if ($userChoice > $rando) {
+		echo "lower!" . PHP_EOL;
+	} else {
+		echo "higher!" . PHP_EOL;
+	}
+
+} while ($userChoice != $rando);
+again();
 }
+function again() {
+	fwrite(STDOUT, 'Play again? Y or N ');
+		$againChoice = fgets(STDIN);
+		if (trim($againChoice) == "Y") {
+		game();
+	};
+}
+game();
