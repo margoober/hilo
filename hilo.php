17d6624e984a3cf09dd2e874b21eeb930d9fc35a
Margot McMahon <andlmcm@Andrews-MacBook-Air.local>
2016-02-29 11:19:17 -0600

    Basic structure of HiLo -- response conditional

--- hilo.php
+++ hilo.php
@@ -1,2 +1,11 @@
 <?PHP
-echo "test" . PHP_EOL;
+$rando = rand(1, 100);
+echo "Comp chosed {$rando}" . PHP_EOL;
+$userChoice;
+if ($userChoice == $rando) {
+	echo "BULLSEYE!" . PHP_EOL;
+} else if ($userChoice > $rando) {
+	echo "Lower!" . PHP_EOL;
+} else {
+	echo "Higher!" . PHP_EOL;
+}
